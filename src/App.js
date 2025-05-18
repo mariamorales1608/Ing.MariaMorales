6b5952bf048b63357f0c3c5ae115b895c894b3cb
maria morales <mafe16082@gmail.com>
2025-05-18 06:55:43 -0400

    tercer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Esto es nuevo
         </p>
         <a
           className="App-link"
